dc4c16da101a6f77964a4bfde88f9cefa873a12d
Justin Selfridge <jselfridge@gmail.com>
2017-06-01 05:21:52 -0400

    Added 'spi_master' and 'spi_slave' modules to a new directory 'spi' within the 'Sandbox' directory.

---
+++ Sandbox/spi/spi_master/spi_master_tb.v
@@ -0,0 +1,19 @@
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+// spi_master_tb.v
+// Test bench for the 'spi_master' module.
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+module spi_master_tb ();
+
+  // Assign test stimulus at certain points
+  initial begin
+
+    $finish;
+
+  end
+
+endmodule
+
+
+
---
+++ Sandbox/spi/spi_slave/spi_slave_tb.v
@@ -0,0 +1,19 @@
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+// spi_slave_tb.v
+// Test bench for the 'spi_slave' module.
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+module spi_slave_tb ();
+
+  // Assign test stimulus at certain points
+  initial begin
+
+    $finish;
+
+  end
+
+endmodule
+
+
+
