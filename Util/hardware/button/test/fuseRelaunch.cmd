6ed0eb67391a51652f1c6e5eeb1a9e7a21f2154d
Justin Selfridge <jselfridge@gmail.com>
2017-06-09 06:29:52 -0400

    Moved 'button' module into 'Util' directory.

---
+++ Util/hardware/button/test/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "/home/justin/Documents/FPGA/Util/hardware/button/test/button_tb_isim_beh.exe" -prj "/home/justin/Documents/FPGA/Util/hardware/button/test/button_tb_beh.prj" "work.button_tb" "work.glbl" 
